4d3af9a3cf5e1bb8ada219d5e001dd46434efd67
CTomm <chloe.tommasi@ens-lyon.fr>
2020-02-01 12:40:07 +0100

    Avec les buffers

--- evaluation/map.js
+++ evaluation/map.js
@@ -5,9 +5,6 @@ var osmAttrib = 'Map data OpenStreetMap contributors' ;
 var osm=new L.TileLayer(osmUrl, {attribution : osmAttrib}). addTo(map);
 map.setView([45.76, 4.85 ], 12.5);
 
-
-
-
 // Metro : Ajouter les lignes de metro, changer le couleur pour chaque ligne 
 
 var metro = L.geoJSON(ligne_metro, {
@@ -25,7 +22,6 @@ var metro = L.geoJSON(ligne_metro, {
 
 
 
-
 // Station auto-partage : Create a icon to use with a GeoJSON (Auto.js) layer instead of the default blue marker. 
 
 // replace Leaflet's default blue marker with my icon
@@ -40,27 +36,34 @@ function createCustomIcon (feature, latlng) {
 }
 
 
-// J'ai simplifié ici un peu ;) -> j'ai mis directement la fonction createCustomIcon dans l'appel du GeoJSON l.62
+// Mettre un buffer puis l'enlever au survol de la souris
+function buffer (feature) {
+  buf = L.circle(feature.latlng,300,{
+          color: "#ECD444",
+          fillColor: "#F4E285"
+      }).addTo(map);
+}
+
+function debuffer (feature) {
+  buf.remove(map);
+}
+
+
+// J'ai simplifié ici un peu ;) -> j'ai mis directement la fonction createCustomIcon dans l'appel du GeoJSON
 // create an options object that specifies which function will called on each feature
 // let myLayerOptions = {
 //   pointToLayer: createCustomIcon
 // };
 
 
-// Station auto-partage : Ajourter pop-up pour afficheir l'adresse:
+// Station auto-partage : Ajouter pop-up pour afficher l'adresse:
 // probleme : j'arrive pas à faire sortit le champ de "adreee"
-
 // -> j'ai fait une fonction onEachFeature pour qu'il aille chercher pour chaque entité son adresse
+// j'y ai mis aussi le buffer au survol de la souris
 function onEachFeature(feature, layer) {
-         layer.bindPopup("<strong> Adresse </strong>: "+feature.properties.adresse);
-        //  layer.on('click', function(){
-        //     map.fitBounds(layer.getBounds()),
-        //     elem.innerHTML=("Population de moins de 20 ans : " +feature.properties._pop20 + "<br> Pourcentage : " + getPourcentage(feature.properties._pmun13,feature.properties._pop20))
-        //  });
-        //  layer.on('mouseover', function(feature){
-        //     layer.setStyle(highlight)
-        //  });
-        // layer.on({mouseout: resetHighlight});
+        layer.bindPopup("<strong> Adresse </strong>: "+feature.properties.adresse);
+        layer.on({mouseover: buffer});
+        layer.on({mouseout: debuffer});
 };  
 
 
@@ -70,30 +73,42 @@ var station_layer =L.geoJSON(station_auto,{
   }).addTo(map);
 
 
-// var adresse_station = [];
-// for (i in station_auto["features"]) {
-//   nom = station_auto["features"][i]["properties"]["nom"];
-//   adresse = station_auto["features"][i]["properties"]["adresse"];
-// };
-//station_layer.bindPopup('Nom : ' + nom + '<br>Adresse : ' + adresse);
-//==> Plus besoin du coup vu que c'est dans le onEachFeature
-
+/// ------------- ANCIENNE FAÇON DE FAIRE LES BUFFERS -------------------
+// c'était pas au survol de la souris donc ça ne répondait pas à la consigne, je le laisse quand même vu que j'ai l'ai débuggé, ça peut toujours servir
 
 // Create a buffer with a GeoJSON layer :
 
-var buffer300 = [];
-for (let feature of station_auto["features"]) {
-	let coor = feature["geometry"]["coordinates"];
-	buffer300.push([coor[1], coor[0]]);
-}
+// var buffer300 = [];
+// for (let feature of station_auto["features"]) {
+// 	let coor = feature["geometry"]["coordinates"];
+// 	buffer300.push([coor[1], coor[0]]);
+// }
 
-for (var i=0 ; i < buffer300.length ; i++) {
-	var buffer_layer = L.circle(buffer300[i],300,{
-					color: "#ECD444",
-          fillColor: "#F4E285"
-      }).addTo(map);
-}
+// //Pour régler le pb j'ai fait un groupe de couche, comme ça tous les buffers sont regroupés
+// var buffer = L.layerGroup()
 
+// for (var i=0 ; i < buffer300.length ; i++) {
+// 	buffer.addLayer(L.circle(buffer300[i],300,{
+// 					color: "#ECD444",
+//           fillColor: "#F4E285"
+//       })).addTo(map);
+// }
+
+// ----------- TAUX DE CHOMAGE PAR IRIS -------------------
+
+
+var iris = L.geoJSON(ligne_metro, {
+    style: function(feature) {
+        switch (feature.properties.ligne) {
+            case 'A': return {color: "#AF125A"};
+            case 'B':   return {color: "#4D87DB"};
+            case 'C':   return {color: "#EC9A29"};
+            case 'D':   return {color: "#0F8B8D"};
+            case 'F1':   return {color: "#8CC7A1"};
+            case 'F2':   return {color: "#8CC7A1"};
+        }
+    }
+}).addTo(map);
 
 
 // Selection de Couches : Afficher des donnees - Gestionnaire de couches :
@@ -105,7 +120,7 @@ var baseLayers = {
 var overlays = {
 	"ligne de métro" : metro,
 	"Station" : station_layer,
-	"300 metre" : buffer_layer,
+	//"300 metre" : buffer, (pour les anciens buffers -> on met directement le groupe de couches dans le overlays)
 };
 
 L.control.layers (baseLayers, overlays). addTo(map);
